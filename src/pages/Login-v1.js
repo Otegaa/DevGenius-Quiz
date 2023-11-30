6df0a519b7481bb8c9e5098fa799f6bf95cb3522
otegaa <ekiugbotega@gmail.com>
2023-11-30 22:49:37 +0100

    Implement password validation for user

---
+++ src/pages/Login-v1.js
@@ -0,0 +1,56 @@
+import { useAuth } from 'contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setIsUsername] = useState('');
+  const [email, setIsEmail] = useState('');
+  const [password, setIsPassword] = useState('');
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ((username, email, password)) login(username, email, password);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) navigate('/languages', { replace: true });
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Please login to continue</h3>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        placeholder="username"
+        id="username"
+        value={username}
+        onChange={(e) => setIsUsername(e.target.value)}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        placeholder="email"
+        id="email"
+        value={email}
+        onChange={(e) => setIsEmail(e.target.value)}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        placeholder="password"
+        id="password"
+        value={password}
+        onChange={(e) => setIsPassword(e.target.value)}
+      />
+      <button>Login</button>
+    </form>
+  );
+};
+export default Login;
